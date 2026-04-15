27a390f8a26e471ce13fc25042ee186f3cd4d50f
laboratoriumsiber-26 <laboratoriumsiber@syekhnurjati.ac.id>
2026-04-15 09:49:53 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxtJG6-0AMD17UQf97Wb3IkjyrD29LmswlGajh2eMEseuO55AWqjrGtPpn_4ORT7JUyKA/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzhZrDLlOT9dnoZaBxjmH1w-Pr5Ud4j-WSip6CDtBRROBQshs95u9iGRdtMattMk8vL/exec';
 const CACHE_KEY = 'podcastData';
 const CACHE_DURATION = 5 * 60 * 1000;
 
