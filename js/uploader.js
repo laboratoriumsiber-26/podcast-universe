34805d64a60820467b31322f231d70e6e3141d07
laboratoriumsiber-26 <laboratoriumsiber@syekhnurjati.ac.id>
2026-04-22 14:59:56 +0700

    Update uploader.js

--- js/uploader.js
+++ js/uploader.js
@@ -1,5 +1,5 @@
 // ==================== KONFIGURASI ====================
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwb6SzDPRYCZkbereZIxH1eKPgEfzn3Tj-By7vhtow5vrOIXCinY3Z7M3UJ6E1oOmoQ1A/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxJUtjkkB3zW9lh-9O4uHOflZC5cfy9lKFuT-DuRh5ktLog56HJiSEt8zMRbc3cEygwng/exec';
 const urlParams = new URLSearchParams(window.location.search);
 const podcastId = urlParams.get('podcast');
 if (!podcastId) {
