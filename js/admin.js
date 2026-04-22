a6c461002ffc4b4b44af63e64bad2b861d02b5bd
laboratoriumsiber-26 <laboratoriumsiber@syekhnurjati.ac.id>
2026-04-22 14:58:53 +0700

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,6 @@
 // js/admin.js
 
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwb6SzDPRYCZkbereZIxH1eKPgEfzn3Tj-By7vhtow5vrOIXCinY3Z7M3UJ6E1oOmoQ1A/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxJUtjkkB3zW9lh-9O4uHOflZC5cfy9lKFuT-DuRh5ktLog56HJiSEt8zMRbc3cEygwng/exec';
 const CACHE_KEY = 'podcastData';
 const CACHE_DURATION = 5 * 60 * 1000;
 
