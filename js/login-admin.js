3590466f97ee413f9273758b7ccf884540442e49
laboratoriumsiber-26 <laboratoriumsiber@syekhnurjati.ac.id>
2026-04-22 16:29:14 +0700

    Refactor login-admin.js with updated settings URL

--- js/login-admin.js
+++ js/login-admin.js
@@ -1,7 +1,7 @@
 // js/login.js
 
 // ==================== KONFIGURASI ====================
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyXQI5sF8ZErj1g2okMCE_KLVi91vljcUBJbcdk2fFhydM5tqzAO_B3H_PIEpC_WxA_Hg/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzuTNChjKRB5JHP5lxbZB8qqRwLZHUpTxWyldX2UpFNQyyPIXatGZzUySlbLHjFO0XyBw/exec';
 const SETTINGS_CACHE_KEY = 'adminSettings';
 const CACHE_DURATION = 5 * 60 * 1000; // 5 menit
 
